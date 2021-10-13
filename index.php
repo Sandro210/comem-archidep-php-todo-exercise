7dd8901d8782ce7372c9447185395b289e7d63de
TanyaKemmPere <tanya.kemmpereira@heig-vd.ch>
2021-10-13 12:53:29 +0200

    mise a jour du SelectQuery

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ define('BASE_URL', '/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
 define('DB_USER', 'todolist');
-define('DB_PASS', 'Piccio-san126');
+define('DB_PASS', 'exoArchiDep!');
 define('DB_NAME', 'todolist');
 define('DB_HOST', '127.0.0.1');
 define('DB_PORT', '3306');
@@ -60,7 +60,7 @@ if (isset($_POST['action'])) {
 
       $id = $_POST['id'];
       if(is_numeric($id)) {
-        $deleteQuery = ''; // IMPLEMENT ME
+        $deleteQuery = 'DELETE FROM todo where id=' . $id; // IMPLEMENT ME
         if(!$db->query($deleteQuery)) {
           die(print_r($db->errorInfo(), true));
         }
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = 'Select * From ORDER BY created-at DESC'; 
+$selectQuery = 'Select * From todo ORDER BY created_at DESC'; 
 $items = $db->query($selectQuery);
 ?>
 
